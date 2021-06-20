efcbe39326308169686fb53cfe8375cacb7a7945
DivadNojnarg <dgranjon@ymail.com>
2021-06-20 20:13:43 +0200

    reactivate

--- R/app_ui.R
+++ R/app_ui.R
@@ -108,9 +108,9 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  # add_resource_path(
-  #  "golem", app_sys("app/www/golem")
-  # )
+  add_resource_path(
+    "golem", app_sys("app/www/golem")
+  )
 
   add_resource_path(
     "www", app_sys("app/www")
