8bae678f1cb9a895bf9177133bf62fa2de76455b
DivadNojnarg <dgranjon@ymail.com>
2021-06-21 19:52:05 +0200

    try out to add shortcut

--- R/app_server.R
+++ R/app_server.R
@@ -7,4 +7,14 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   output$val <- renderPrint(input$stepper)
+  
+  observeEvent(session$clientData$url_search, {
+    query <- parseQueryString(session$clientData$url_search)
+    
+    # Ways of accessing the values
+    if (as.numeric(query$foo) == 1) {
+      f7Toast(text = "Plop")
+    }
+  })
+  
 }
